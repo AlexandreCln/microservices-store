d161cf5671cfd749fde15602704fbafc48d09613
Alexandre Coulon <55137382+AlexandreCln@users.noreply.github.com>
2026-03-16 21:35:30 +0100

    feat: add catalog-service microservice

---
+++ catalog-service/src/main/java/fr/microservices_store/catalog_service/Application.java
@@ -0,0 +1,13 @@
+package fr.microservices_store.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ catalog-service/src/test/java/fr/microservices_store/catalog_service/TestApplication.java
@@ -0,0 +1,11 @@
+package fr.microservices_store.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
