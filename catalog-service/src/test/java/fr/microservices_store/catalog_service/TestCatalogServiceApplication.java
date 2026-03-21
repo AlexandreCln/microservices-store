071174057da84e3c9bd7a96bc166971f8b958421
Alexandre Coulon <55137382+AlexandreCln@users.noreply.github.com>
2026-03-21 15:20:01 +0100

    refactor: rename TestApplication class to TestCatalogServiceApplication

--- catalog-service/src/test/java/fr/microservices_store/catalog_service/TestCatalogServiceApplication.java
+++ catalog-service/src/test/java/fr/microservices_store/catalog_service/TestCatalogServiceApplication.java
@@ -2,7 +2,7 @@ package fr.microservices_store.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestApplication {
+public class TestCatalogServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(CatalogServiceApplication::main)
