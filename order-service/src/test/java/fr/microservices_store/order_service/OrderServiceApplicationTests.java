ad230927c52f20a3f494745f7d83a8fc1bc52ddc
Alexandre Coulon <55137382+AlexandreCln@users.noreply.github.com>
2026-03-21 15:34:09 +0100

    test: add AbstractIT base class and RestAssured for Order Service integration tests

--- order-service/src/test/java/fr/microservices_store/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/fr/microservices_store/order_service/OrderServiceApplicationTests.java
@@ -1,12 +1,8 @@
 package fr.microservices_store.order_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class OrderServiceApplicationTests {
+class OrderServiceApplicationTests extends AbstractIT {
 
 	@Test
 	void contextLoads() {
